dde59f6d8e64ba26c0ad21cf04b309e268d81ce8
Richard <richard@dreamsilver.local>
2016-02-22 23:42:28 +0000

    Add documentation

--- src/libsudoku/include/sudoku/puzzle.h
+++ src/libsudoku/include/sudoku/puzzle.h
@@ -21,17 +21,42 @@ namespace Sudoku
 
 using PuzzleDigit = std::uint8_t;
 
+/// <summary>
+/// The width of a sudoku puzzle
+/// </summary>
 static const int PuzzleWidth = 9;
+/// <summary>
+/// The height of a sudoku puzzle
+/// </summary>
 static const int PuzzleHeight = 9;
 
+/// <summary>
+/// The width of a sub-grid within a sudoku puzzle
+/// </summary>
 static const int SubGridWidth = 3;
+/// <summary>
+/// The height of a sub-grid within a sudoku puzzle
+/// </summary>
 static const int SubGridHeight = 3;
 
 /* ************************************************************************* */
 
+/// <summary>
+/// A class representing the sudoku puzzle
+/// </summary>
+/// <remarks>
+/// A Cell with a value of 0 is considered blank.
+/// </remarks>
 class Puzzle : public TwoDArray<PuzzleDigit, PuzzleWidth, PuzzleHeight>
 {
 public:
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <remarks>
+    /// The Puzzle is initially set to blank
+    /// </remarks>
     Puzzle();
     ~Puzzle() = default;
 
