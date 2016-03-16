84178885017952a859fe39cc05ff6b8025c78dd8
Richard <richard@dreamsilver.local>
2016-03-16 22:36:47 +0000

    Add ISolver interface refs #58

---
+++ src/libsudoku/src/solver/isolver.h
@@ -0,0 +1,54 @@
+/*
+ * isolver.h
+ *
+ * Interface definition for the Sudoku ISolver interface
+ *
+ * (c) Richard Oak 2016
+ */
+ 
+/* ************************************************************************* */
+
+#pragma once
+
+#include "sudoku/puzzledigit.h"
+
+namespace Sudoku
+{
+
+/* ************************************************************************* */
+
+class SolverElem;
+
+/* ************************************************************************* */
+
+/// <summary>
+/// Interface for the Solver
+/// </summary>
+/// <remarks>
+/// A Solver is an algorithm for trying to solve a cell in the grid
+/// </remarks>
+class ISolver
+{
+public:
+
+    /// <summary>
+    /// virtual destructor
+    /// </summary>
+    virtual ~ISolver();
+    
+    /// <summary>
+    /// Find a solution for the cell
+    /// </summary>
+    /// <param name="cell">The cell to solve</param>
+    /// <returns>The solution, or 0 if no clear solution can be found</returns>
+    virtual PuzzleDigit GetSolution(const Sudoku::SolverElem &cell) = 0;
+
+protected:
+
+private:
+
+};
+
+/* ************************************************************************* */
+
+}
