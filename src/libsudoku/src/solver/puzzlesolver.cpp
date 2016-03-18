b6e4a00e17cb8ec9074f1c202f1598f72f25f3b7
Richard <richard@dreamsilver.local>
2016-03-18 23:10:49 +0000

    Add docs and tests for solver fixes #58

--- src/libsudoku/src/solver/puzzlesolver.cpp
+++ src/libsudoku/src/solver/puzzlesolver.cpp
@@ -8,6 +8,9 @@
  
 /* ************************************************************************* */
 
+#include <vector>
+#include <memory>
+
 #include "puzzlesolver.h"
 
 #include "sudoku/puzzle.h"
@@ -16,9 +19,100 @@
 #include "zone/row.h"
 #include "zone/subgrid.h"
 
+#include "solver/cellsolver.h"
+#include "solver/zonesolver.h"
+
 /* ************************************************************************* */
 
 using Sudoku::PuzzleSolver;
+
+/* ************************************************************************* */
+/* ************************************************************************* */
+
+namespace
+{
+    
+/* ************************************************************************* */
+
+/// <summary>
+/// Copy all of the cells from the puzzle into the solver 
+/// </summary>
+/// <param name="solver">[out] The puzzle solver being setup</param>
+/// <param name="puzzle">The puzzle that is being solved</param>
+void SetupBoard(PuzzleSolver &solver, const Sudoku::Puzzle &puzzle)
+{
+    for (auto y = 0; y < Sudoku::PuzzleHeight; y++)
+    {
+        for (auto x = 0; x < Sudoku::PuzzleWidth; x++)
+        {
+            Sudoku::PuzzleDigit digit = puzzle.Cell(x, y);
+            
+            if (digit.get() != 0)
+            {
+                solver.Cell(x, y).Solve(digit);
+            }
+        }
+    }    
+}
+
+/* ************************************************************************* */
+
+/// <summary>
+/// Try and solve each of the cells in the puzzle, using the supplied ISolver
+/// </summary>
+/// <param name="solver">The puzzle solver</param>
+/// <param name="solvers">The methods for solving a cell</param>
+/// <returns>
+/// If a cell was solved, return true. Otherwise return false as there's no new
+/// information and we'll not be able to make anymore headway in solving the puzzle
+/// </returns>
+bool SolveBoard(PuzzleSolver &solver, const std::vector<std::unique_ptr<Sudoku::ISolver>> &solvers)
+{
+    bool result = false;
+    
+    for (auto y = 0; y < Sudoku::PuzzleHeight; y++)
+    {
+        for (auto x = 0; x < Sudoku::PuzzleWidth; x++)
+        {
+            for (auto &solve : solvers)
+            {
+                Sudoku::PuzzleDigit digit = solve->GetSolution(solver.Cell(x, y));
+                
+                if (digit.get() != 0)
+                {
+                    solver.Cell(x, y).Solve(digit);
+                    result = true;    
+                }
+            }
+        }
+    }
+    
+    return result;
+}
+    
+/* ************************************************************************* */
+
+/// <summary>
+/// Copy all of the cells from the puzzle into the solver 
+/// </summary>
+/// <param name="solver">The puzzle solver containing the solution</param>
+/// <param name="puzzle">[out] Where to put the solution</param>
+void GetSolution(const PuzzleSolver &solver, Sudoku::Puzzle &puzzle)
+{
+    for (auto y = 0; y < Sudoku::PuzzleHeight; y++)
+    {
+        for (auto x = 0; x < Sudoku::PuzzleWidth; x++)
+        {
+            Sudoku::PuzzleDigit digit = solver.Cell(x, y).Solution();
+            
+            puzzle.Cell(x, y) = digit;
+        }
+    }    
+}
+    
+/* ************************************************************************* */
+
+} // anonymous namespace
 
 /* ************************************************************************* */
 /* ************************************************************************* */
@@ -41,7 +135,23 @@ PuzzleSolver::PuzzleSolver()
 
 Sudoku::Puzzle PuzzleSolver::Solve(const Sudoku::Puzzle &puzzle)
 {
-    return puzzle;
+    std::vector<std::unique_ptr<ISolver>> solvers;
+    
+    solvers.push_back(std::make_unique<CellSolver>());
+    solvers.push_back(std::make_unique<ZoneSolver>());
+
+    SetupBoard(*this, puzzle);
+
+    while (SolveBoard(*this, solvers))
+    {
+        // loop around, until there's nothing more we can solve
+        ;
+    }
+    
+    Sudoku::Puzzle solution;
+    GetSolution(*this, solution);
+    
+    return solution;
 }
 
 /* ************************************************************************* */
--- src/libsudoku/test/solver/puzzlesolvertest.cpp
+++ src/libsudoku/test/solver/puzzlesolvertest.cpp
@@ -26,16 +26,71 @@ private:
 };
 
 /* ************************************************************************* */
+
+// http://www.theguardian.com/lifeandstyle/2016/mar/14/sudoku-3382-easy
+Sudoku::Puzzle Easy3382()
+{
+    Sudoku::Puzzle puzzle;
+    
+    puzzle.Cell(7,0) = 9;
+    puzzle.Cell(8,0) = 7;
+    puzzle.Cell(3,1) = 5;
+    puzzle.Cell(4,1) = 7;
+    puzzle.Cell(7,1) = 8;
+    puzzle.Cell(3,2) = 4;
+    puzzle.Cell(4,2) = 2;
+    puzzle.Cell(5,2) = 6;
+    
+    puzzle.Cell(1,3) = 8;
+    puzzle.Cell(2,3) = 1;   
+    puzzle.Cell(7,3) = 4;
+    puzzle.Cell(1,4) = 7;
+    puzzle.Cell(2,4) = 6;
+    puzzle.Cell(6,4) = 3;
+    puzzle.Cell(8,4) = 2;
+    puzzle.Cell(2,5) = 2;
+    puzzle.Cell(5,5) = 5;
+    puzzle.Cell(6,5) = 9;
+    
+    puzzle.Cell(4,6) = 5;
+    puzzle.Cell(5,6) = 8;
+    puzzle.Cell(8,6) = 1;
+    puzzle.Cell(0,7) = 1;
+    puzzle.Cell(1,7) = 3;
+    puzzle.Cell(3,7) = 7;
+    puzzle.Cell(0,8) = 7;
+    puzzle.Cell(4,8) = 1;
+    puzzle.Cell(6,8) = 5;
+    
+    return puzzle;
+}
+
+/* ************************************************************************* */
 /* ************************************************************************* */
 
 TEST_F(PuzzleSolverTest, Solve1)
 {
-    Sudoku::Puzzle puzzle;
+    std::string expected{
+        "654381297\n"
+        "213579486\n"
+        "897426153\n"
+        "981237645\n"
+        "576894312\n"
+        "342165978\n"
+        "429658731\n"
+        "135742869\n"
+        "768913524\n"
+    };
+    Sudoku::Puzzle puzzle = Easy3382();
     PuzzleSolver solver;
     
     Sudoku::Puzzle solution = solver.Solve(puzzle);
-    
-    // for now, just code coverage
+
+    std::stringstream stream;
+
+    stream << solution;
+
+    ASSERT_EQ(expected, stream.str());
 }
 
 /* ************************************************************************* */
